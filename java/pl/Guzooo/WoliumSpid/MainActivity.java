3d8f825c1de0687622b6ab035e710cced406e29f
Guzooo <guzooo-guzooo@o2.pl>
2021-07-29 09:12:00 +0200

    0.0.3
    Dodano Activity ustawień
    Dodano obsługę zmiany motywu oraz Activity na całym ekranie

--- java/pl/Guzooo/WoliumSpid/MainActivity.java
+++ java/pl/Guzooo/WoliumSpid/MainActivity.java
@@ -1,26 +1,61 @@
 package pl.Guzooo.WoliumSpid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import pl.Guzooo.Base.Elements.BusinessCard;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.OnApplyWindowInsetsListener;
+import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import pl.Guzooo.Base.Elements.BusinessCard;
+import pl.Guzooo.Base.ModifiedElements.GActivity;
+import pl.Guzooo.Base.Utils.FullScreenUtils;
+import pl.Guzooo.Base.Utils.ThemeUtils;
+
+public class MainActivity extends GActivity {
+
+    View addFAB;
+    View settingsFAB;
+
+    @Override
+    public int getBottomPadding() {
+        int bottom = addFAB.getHeight();
+        bottom += getMarginBiggest() *2;
+        return bottom;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ThemeUtils.setTheme(this);
         setContentView(R.layout.activity_main);
 
         initialization();
+        setFullScreen();
         setBusinessCard();
         setProfilesRecycler();
     }
 
-    private void initialization(){
+    public void onClickAddProfile(View v){
 
+    }
+
+    public void onClickSettings(View v){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+    private void initialization(){
+        addFAB = findViewById(R.id.add_fab);
+        settingsFAB = findViewById(R.id.settings_fab);
+    }
+
+    private void setFullScreen(){
+        View mainScroll = findViewById(R.id.main_scroll);
+        FullScreenUtils.setUIVisibility(mainScroll);
+        FullScreenUtils.setPaddings(mainScroll, this);
+        FullScreenUtils.setApplyWindowInsets(addFAB, getWindowsInsetsListener());
     }
 
     private void setBusinessCard(){
@@ -31,5 +66,32 @@ public class MainActivity extends AppCompatActivity {
 
     private void setProfilesRecycler(){
 
+    }
+
+    private OnApplyWindowInsetsListener getWindowsInsetsListener(){
+        return new OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets(View v, WindowInsetsCompat insets) {
+                setInsets(insets);
+                setAddFabSpacing(insets);
+                setSettingsFabSpacing(insets);
+                return insets;
+            }
+        };
+    }
+
+    private void setAddFabSpacing(WindowInsetsCompat insets){
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) addFAB.getLayoutParams();
+        layoutParams.bottomMargin = insets.getSystemWindowInsetBottom() + getMarginBiggest();
+    }
+
+    private void setSettingsFabSpacing(WindowInsetsCompat insets){
+        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) settingsFAB.getLayoutParams();
+        layoutParams.bottomMargin = insets.getSystemWindowInsetBottom() + getMarginBiggest();
+        layoutParams.leftMargin = insets.getSystemWindowInsetLeft() + getMarginBiggest();
+    }
+
+    private int getMarginBiggest(){
+        return getResources().getDimensionPixelOffset(R.dimen.margin_biggest);
     }
 }
---
+++ java/pl/Guzooo/WoliumSpid/SettingsActivity.java
@@ -0,0 +1,36 @@
+package pl.Guzooo.WoliumSpid;
+
+import android.os.Bundle;
+import android.view.View;
+
+import pl.Guzooo.Base.ModifiedElements.GActivity;
+import pl.Guzooo.Base.Utils.FullScreenUtils;
+import pl.Guzooo.Base.Utils.SettingsUtils;
+
+public class SettingsActivity extends GActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        setFullScreen();
+        setActionBar();
+        setFragment();
+    }
+
+    private void setFullScreen(){
+        View mainScroll = findViewById(R.id.main_scroll);
+        FullScreenUtils.setUIVisibility(mainScroll);
+        FullScreenUtils.setPaddings(mainScroll, this);
+    }
+
+    private void setActionBar(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setFragment(){
+        View content = findViewById(R.id.content);
+        SettingsUtils.setSettingsFragment(content, this);
+    }
+}
